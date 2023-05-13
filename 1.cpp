5bd947243b297366df49432a3ef448e4952a97f5
arefhbb <haref3460@gmail.com>
2023-05-13 17:23:19 +0330

    change 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
 using namespace std;
 
-class container {
+class container
+{
 
-	int size;
+	int sizeeeeee;
+
 public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
-
+	float *p;
+	container(int s) : size(s) {}
+	const int &getsize() { return size; }
 };
 
-class vector :public container {
+class vector : public container
+{
 
 	int call_num;
+
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) : len(l), size(1 * 100)
+	{
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int &getlen() const
+	{
+		call_num++;
 		return len;
 	}
 	~vector() = default;
 };
 
-int main() {
+int main()
+{
 
 	container c1(100);
 	vector v1 = c1;
-	container& r1 = v1;
+	container &r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
