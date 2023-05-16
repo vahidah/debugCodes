e5f3dac9693e2d7f727c9e3c7b2c5e2255bf64c6
Mahsan Hadi <mhsnhadi@yahoo.com>
2023-05-17 00:47:40 +0330

    1 and 2 debug completed

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,48 @@
 #include <iostream>
 using namespace std;
 
-class container {
-
+class container
+{
 	int size;
-public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
 
+public:
+	float *p;
+	container(int s) : size(s) {}
+	int &getsize() { return size; }
 };
 
-class vector :public container {
-
+class vector : public container
+{
 	int call_num;
+
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) : len(l), container(1 * 100)
+	{
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int &getlen()
+	{
+		call_num++;
 		return len;
 	}
-	~vector() = default;
+	~vector() { delete p; };
+	vector(const container &c) : container(c)
+	{
+		p = new float;
+		len = 0;
+	}
 };
 
-int main() {
-
+int main()
+{
 	container c1(100);
 	vector v1 = c1;
-	container& r1 = v1;
+	container &r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2(100);
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
