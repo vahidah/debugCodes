df640535eb45625081df033554ecd8c045a03b69
pouya <kingmarhom>
2023-05-16 21:26:53 +0330

    edit

--- 1.cpp
+++ 1.cpp
@@ -8,21 +8,22 @@ public:
 	float* p;
 	container(int s) :size(s){}
 	const int& getsize() { return size;}
-
+    const int& getsize() const{ return size;}
 };
 
 class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l),container(1 * 100){
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
+	int& getlen()  {
 		call_num ++;
 		return len;
 	}
+    vector(const container&a) :len(10), container(a.getsize()){}
 	~vector() = default;
 };
 
@@ -32,9 +33,9 @@ int main() {
 	vector v1 = c1;
 	container& r1 = v1;
 	container c2 = 100;
-	c2.getsize() = 20;
+	c2.getsize() ;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2 = (vector)100;
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
