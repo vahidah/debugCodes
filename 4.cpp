6012a28dbe1302a11eb312d4b31538c5232c4c68
vania <vania@gmail.com>
2023-05-17 21:09:11 +0330

    file4 is edited and compiled successfuly.

--- 4.cpp
+++ 4.cpp
@@ -6,4 +6,8 @@ int main()
     float *ptr2 = ptr1 + 3;
     printf("%f", *ptr2 - *ptr1);
     return 0;
-}
+}/*this program first build a float_array contain of 
+5 memberes and then we have pointer1 that have the address 
+of the first house of array and the second pointer that have the
+address of the fourth house of array and then we print the defference
+between the tow number that these these two pointers point to and the output is 78*/
