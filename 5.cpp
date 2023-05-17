4469ed4a6bb670c22662aa1ea757dd680d606b40
Zahra Tavakoli <z.tavakoli@ec.iut.ac.ir>
2023-05-17 19:52:19 +0330

    5.cpp debuged

--- 5.cpp
+++ 5.cpp
@@ -2,9 +2,9 @@
 int main()
 {
     int arr[] = { 10, 20, 30, 40, 50, 60 };
-    int *ptr1 = arr;
-    int *ptr2 = arr + 5;
-    printf("%d\n", (*ptr2 - *ptr1));
-    printf("%c", (char)(*ptr2 - *ptr1));
+    int *ptr1 = arr;//ptr1==&arr[0]
+    int *ptr2 = arr + 5;//ptr2==&arr[5]
+    printf("%d\n", (*ptr2 - *ptr1));// prints 50 + new line
+    printf("%c", (char)(*ptr2 - *ptr1));// prints 2
     return 0;
 }
