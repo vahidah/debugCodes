d305613fb5ecb171d21bc71c72fcf24286761d5a
Zahra Tavakoli <z.tavakoli@ec.iut.ac.ir>
2023-05-17 19:54:00 +0330

    7.cpp debuged

--- 7.cpp
+++ 7.cpp
@@ -2,9 +2,9 @@
 int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5 };
-	int *p = arr;
-	++*p;
-	p += 2;
-	printf("%d", *p);
+	int *p = arr;//p==&arr[0]
+	++*p;//arr[0]=2;
+	p += 2;//p==&arr[2]
+	printf("%d", *p);//prints 3
 	return 0;
 }
